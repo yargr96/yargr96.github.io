255a5fceac9a5b657fbe4b067b673c12ff7313cd
yargr96 <yargr96@gmail.com>
2017-12-06 14:50:12 +0300

    asdada

--- agency_landing/js/app.js
+++ agency_landing/js/app.js
@@ -1,30 +1,46 @@
-function showCase() {
-	var popup = $('#case-1');
-	popup.style.display = "block";
-	window.setTimeout(function() {
-		popup.addClass('popup-case_active')
-	}, 0);
-	
-}
-function hideCase() {
-	var popup = $('.popup-case_active');
-	popup.removeClass('popup-case_active');
-	popup.css('display', 'none');
-}
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
+	//modals
+	$('a#show_case-1').click(function(event) {
+		event.preventDefault();
+		$('#case-1').css('display', 'block')
+					.animate({opacity: 1, left: 0}, 500)
+	});
+	$('a#show_case-2').click(function(event) {
+		event.preventDefault();
+		$('#case-2').css('display', 'block')
+					.animate({opacity: 1, left: 0}, 500)
+	});
+	$('a#show_case-3').click(function(event) {
+		event.preventDefault();
+		$('#case-3').css('display', 'block')
+					.animate({opacity: 1, left: 0}, 500)
+	});
+	$('a#show_case-4').click(function(event) {
+		event.preventDefault();
+		$('#case-4').css('display', 'block')
+					.animate({opacity: 1, left: 0}, 500)
+	});
+	$('a#show_case-5').click(function(event) {
+		event.preventDefault();
+		$('#case-5').css('display', 'block')
+					.animate({opacity: 1, left: 0}, 500)
+	});
+	$('a#show_case-6').click(function(event) {
+		event.preventDefault();
+		$('#case-6').css('display', 'block')
+					.animate({opacity: 1, left: 0}, 500)
+	});
+	// Закрытие окна
+	$('.popup-case_column_close').click(function() {
+		$('.popup-case')
+			.animate({opacity: 0, left: '100%'}, 500,
+				function() {
+					$(this).css('display', 'none');
+				}
+		);
+	});
 
- document.querySelector(".portfolio_item_cover_btn").addEventListener("click", function() {
-
-     var popup = document.querySelector("#case-1");
-
-     popup.style.display = "block";
-
-     window.setTimeout(function() {
-
-         popup.classList.add("popup-case_active")
-
-     }, 0)
-
- })
-
+    $(".popup-case_preview-i2").niceScroll({
+        cursorcolor: "#f5c84a"
+    });
 });
